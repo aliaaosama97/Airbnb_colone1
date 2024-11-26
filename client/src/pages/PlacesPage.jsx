617f92c293d25dde6c85ef2ea40fa3ecbaf396d9
Aliaa osama <aliaaosama97@gmail.com>
2024-11-26 11:42:22 +0200

    last verssion

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -6,7 +6,7 @@ import PlaceImg from "../PlaceImg";
 export default function PlacesPage() {
   const [places,setPlaces] = useState([]);
   useEffect(() => {
-    axios.get('http://localhost:4000/user-places').then(({data}) => {
+    axios.get('/user-places').then(({data}) => {
       setPlaces(data);
     });
   }, []);
